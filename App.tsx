ce425ac4c6314abdadae073c1b1cc3986e03d937
Joel <joelluis73@gmail.com>
2022-04-23 09:42:16 -0300

    install prettier, eslint

--- App.tsx
+++ App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from 'react'
+import { View } from 'react-native'
 import AppLoading from 'expo-app-loading'
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans'
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display'
 
 export default function App() {
- const [fontsLoaded] = useFonts({
-    DMSans_400Regular, 
+  const [fontsLoaded] = useFonts({
+    DMSans_400Regular,
     DMSerifDisplay_400Regular
-  });
+  })
 
-  if(!fontsLoaded) {
+  const bla = 'fkdl'
+
+  if (!fontsLoaded) {
     return <AppLoading />
   }
 
-  return (
-    <View >
-    
-    </View>
-  );
+  return <View></View>
 }
-
